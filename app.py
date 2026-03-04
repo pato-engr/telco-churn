6e725bd772ecc34026d5add8ffc783d3ce084156
pato-engr <vitalaandrew@gmail.com>
2026-03-04 15:37:45 +0100

    app.py got updated with new features

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("Hello, World!")
+from flask import Flask, render_template, request, jsonify
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+model = joblib.load("Churn_Model.pkl")
+
+@app.route("/ predict", methods=["POST"])
+def predict():
+    data = request.json()
+    df = pd.DataFrame([data])
+    prediction = model.predict(df)
+    return jsonify({"prediction": int(prediction[0])})
+
+if __name__ == "__main__":
+    app.run(debug=True)
